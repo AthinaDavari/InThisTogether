9b735b582f8da14d6215264a55e5c7db7b860887
ElenaTzerefou <56511251+ElenaTzerefou@users.noreply.github.com>
2020-12-15 20:29:24 +0200

    Create Customer.java

---
+++ gr.aueb.dmst.isia.inthistogether/src/gr/aueb/dmst/isia/inthistogether/Customer.java
@@ -0,0 +1,82 @@
+package inthistogether;
+
+public class Customer {
+    
+  private String email;
+	private int phone;
+	private String password;
+	private String name;
+  private String surname;
+	
+	
+	public Customer(String email, int phone, String password, String name, String surname) {
+		super();
+		this.email = email;
+		this.phone = phone;
+		this.password = password;
+		this.name = name;
+		this.surname = surname;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public int getPhone() {
+		return phone;
+	}
+
+
+	public void setPhone(int phone) {
+		this.phone = phone;
+	}
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getSurname() {
+		return surname;
+	}
+
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Customer [email=" + email + ", phone=" + phone + ", password=" + password + ", name=" + name
+				+ ", surname=" + surname + "]";
+	}
+
+	
+
+    
+
+}
