d0903a34e4f8d51b58dacf2cf1346c506f099407
Anna Shambel <56415360+AnnaShambel3@users.noreply.github.com>
2020-12-16 00:23:06 +0200

    Create Education.java

---
+++ gr.aueb.dmst.isia.inthistogether/src/gr/aueb/dmst/isia/inthistogether/Education.java
@@ -0,0 +1,59 @@
+package gr.aueb.dmst.isia.inthistogether;
+import java.sql.*;
+
+public class Education {
+
+    private int id;
+    private String customerEmail;
+    private String title;
+    private String typeOfEducation;
+    
+    public Education (int id, String customerEmail, String title, String typeOfEducation) {
+
+		super();
+		this.id = id;
+		this.customerEmail = customerEmail;
+		this.title = title;
+        this.typeOfEducation = typeOfEducation;
+        
+    }
+    
+    public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+    }
+    
+    public String getCustomerEmail() {
+		return customerEmail;
+	}
+
+	public void setCustomerEmail(String customerEmail) {
+		this.customerEmail = customerEmail;
+    }
+
+    public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+    
+    public String getTypeOfEducation() {
+		return typeOfEducation;
+	}
+
+	public void setTypeOfEducation(String typeOfEducation) {
+		this.typeOfEducation = typeOfEducation;
+    }
+    
+    @Override
+	public String toString() {
+		return "Ad [id=" + id + ", customerEmail=" + customerEmail + ", title=" + title + ", typeOfEducation="
+				+ typeOfEducation + "]";
+	}
+
+}
