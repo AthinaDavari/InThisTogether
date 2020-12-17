511e7d055a8144a9ce6d837996fa5665064ca7eb
AthinaDavari <t8180020@aueb.gr>
2020-12-17 02:17:44 +0200

    Add getAds method

--- gr.aueb.dmst.isia.inthistogether/src/gr/aueb/dmst/isia/inthistogether/AdDAO.java
+++ gr.aueb.dmst.isia.inthistogether/src/gr/aueb/dmst/isia/inthistogether/AdDAO.java
@@ -1,8 +1,46 @@
 package gr.aueb.dmst.isia.inthistogether;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AdDAO {
+	
+	public List<Ad> getAds() throws Exception{
+		
+		DB db = new DB();
+		Connection con = null;
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		List<Ad> ads = new ArrayList<Ad>();
+		String getads = "SELECT * FROM Ad";
+
+		try {
+
+			stmt = con.prepareStatement(getads); // initialize stmt insert
+			rs = stmt.executeQuery();
+
+			while(rs.next()) {
+				ads.add(new Ad(rs.getInt("id"), rs.getString("customerEmail"), rs.getDate("startDate"), rs.getDate("endDate"), rs.getString("service"), rs.getString("disability"), rs.getString("description"), rs.getString("location") ));
+			}
+			
+			rs.close();
+			stmt.close();
+
+
+		} catch (Exception e) {
+			throw new Exception(e.getMessage());
+		} finally {
+			try {
+				db.close();
+			} catch (Exception e) {
+
+			}
+		}
+		
+		return ads;
+
+	}
 
 	public void insertAd(Ad ad) throws Exception {
 
