450be0b8d1305e4afd3aa469fc7ec33962b7ee41
AthinaDavari <t8180020@aueb.gr>
2020-12-17 12:57:16 +0200

    Remove id from insert query

--- gr.aueb.dmst.isia.inthistogether/src/gr/aueb/dmst/isia/inthistogether/AdDAO.java
+++ gr.aueb.dmst.isia.inthistogether/src/gr/aueb/dmst/isia/inthistogether/AdDAO.java
@@ -47,20 +47,19 @@ public class AdDAO {
 		DB db = new DB();
 		Connection con = null;
 		PreparedStatement stmt = null;
-		String insertad = "INSERT INTO Ad (id, customerEmail, startDate, endDate, service, disability, description, location) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+		String insertad = "INSERT INTO Ad (customerEmail, startDate, endDate, service, disability, description, location) VALUES (?, ?, ?, ?, ?, ?, ?)";
 
 		try {
 
 			stmt = con.prepareStatement(insertad); // initialize stmt insert
 
-			stmt.setInt(1, ad.getId());
-			stmt.setString(2,ad.getCustomerEmail());
-			stmt.setDate(3,ad.getStartDate());
-			stmt.setDate(4,ad.getEndDate());
-			stmt.setString(5,ad.getService());
-			stmt.setString(6,ad.getDisabity());
-			stmt.setString(7,ad.getDescription());
-			stmt.setString(8,ad.getLocation());
+			stmt.setString(1,ad.getCustomerEmail());
+			stmt.setDate(2,ad.getStartDate());
+			stmt.setDate(3,ad.getEndDate());
+			stmt.setString(4,ad.getService());
+			stmt.setString(5,ad.getDisabity());
+			stmt.setString(6,ad.getDescription());
+			stmt.setString(7,ad.getLocation());
 
 			stmt.executeUpdate();
 
