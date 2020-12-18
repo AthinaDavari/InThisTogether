241f81ed630dbc75ea6d7a17258edc6c250d9f23
Anna Shambel <56415360+AnnaShambel3@users.noreply.github.com>
2020-12-18 18:51:07 +0200

    Add method getAmeaSupporters in AMEASupporterDAO

--- gr.aueb.dmst.isia.inthistogether/src/gr/aueb/dmst/isia/inthistogether/AMEASupporterDAO.java
+++ gr.aueb.dmst.isia.inthistogether/src/gr/aueb/dmst/isia/inthistogether/AMEASupporterDAO.java
@@ -1,4 +1,4 @@
-package gr.aueb.dmst.isia.inthistogether;
+package inthistogether;
 
 import java.sql.*;
 import java.util.ArrayList;
@@ -75,6 +75,57 @@ public class AMEASupporterDAO {
 			}
 		}	
 		
-    } //End of authenticate
+	} //End of authenticate
+	
+	public List<AMEASupporter> getAmeaSupporters() throws Exception {
+				
+		List<AMEASupporter> ameasup =  new ArrayList<AMEASupporter>();
+
+		DB db = new DB();
+		Connection con = null;
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+
+		String sql = "SELECT * FROM ameasupporter;";
+
+		try {
+			
+			con = db.getConnection();
+			stmt = con.prepareStatement(sql);
+			rs = stmt.executeQuery();
+
+			while(rs.next()) {
+				ameasup.add(new AMEASupporter(rs.getString("email"), rs.getString("name"), rs.getString("surname"),
+				rs.getLong("phone"), rs.getString("serviceTown"), rs.getString("serviceArea"),
+				rs.getString("password"), rs.getString("sex"), rs.getDate("birthDate"),
+				rs.getString("languages"), rs.getBoolean("drivingLisence"), rs.getBoolean("carOwner"),
+				rs.getDouble("payPerHour"), rs.getBoolean("available"), rs.getBoolean("monday"),
+				rs.getBoolean("tuesday"), rs.getBoolean("wednesday"), rs.getBoolean("thursday"),
+				rs.getBoolean("friday"), rs.getBoolean("saturday"), rs.getBoolean("sunday"),
+				rs.getBoolean("deaf"), rs.getBoolean("dyslexia"), rs.getBoolean("epilipsy"),
+				rs.getBoolean("autism"), rs.getBoolean("blind"), rs.getBoolean("mobilityImpaired"),
+				rs.getBoolean("down"), rs.getBoolean("learningSupportPrimarySchool"), rs.getBoolean("learningSupportJuniorHighSchool"),
+				rs.getBoolean("learningSupportSeniorHighSchool"), rs.getBoolean("occupationalTherapy"), rs.getBoolean("logotherapy"),			
+				rs.getBoolean("schoolCompanion"), rs.getBoolean("externalCompanion")));
+			}
+
+			rs.close();
+			stmt.close();
+			return ameasup;
+
+		} catch (Exception e) {
+			throw new Exception(e.getMessage());
+
+		} finally {
+
+			try {
+				db.close();
+			} catch (Exception e) {
+
+			}
+
+		}
+		
+	} //End of getUsers
     
 }
